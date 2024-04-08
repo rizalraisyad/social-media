02c6655d3a563dac63e876249c5253c69bb07168
Rizal Muharam <rizal.muharam@siklus.com>
2024-04-08 13:34:33 +0700

    fix unit test

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,13 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFilesAfterEnv: ["<rootDir>/tests/jest.setup.ts"],
+  transform: {
+    "node_modules/variables/.+\\.(j|t)sx?$": "ts-jest"
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!variables/.*)"
+  ],
+  moduleNameMapper: {
+    "^axios$": "axios/dist/node/axios.cjs"
+  }
 };
